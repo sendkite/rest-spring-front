1c6c23877ce00410dd3d6c6de09c8875df9b5b69
sendkite <sendkite1@gmail.com>
2022-01-20 16:19:58 +0900

    matertial ui 적용 ui 개선

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import { Paper, List } from "@material-ui/core";
 import './App.css';
 
 
@@ -15,27 +16,18 @@ class App extends React.Component {
   }
 
   render() {
-    const text = 'JSX 문법을 사용하면 데이터 바인딩이 쉬워요'
-    function test() {
-      return "함수도 동적으로 추가 할 수 있어요";
-    }
 
-    let todoItems = this.state.items.map((item, idx) => (
-      <Todo item={item} key={item.id} />
-    ));
+    let todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          { this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    )
 
-    return(
-      <div className="App">
-        <div className="black-nav">
-          <div>Todo list</div>
-        </div>
-        
-        <h4>{ text }</h4>
-        <h4>{ test() }</h4>
-        <h4>{ todoItems }</h4>
-        
-      </div>
-    );
+    return <div className="App">{ todoItems }</div> ;
   };
 }
 
