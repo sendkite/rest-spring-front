1eaf482952189449bab0f9f08bfbccbe58143e21
sendkite <sendkite1@gmail.com>
2022-01-20 17:17:03 +0900

    Feature: create, read 이벤트 생성

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,17 +5,43 @@ class AddTodo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {item: {title: ""}};
+        this.add = props.add; // add 함수 연결
     }
+
+    // 이벤트 생성
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(thisItem);
+    }
+
+    onButtonClick = () => {
+        this.add(this.state.item);
+        this.setState({ item: { title: "" } });
+      };
+    
+    enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+          this.onButtonClick();
+        }
+    };
 
     render() {
         return(
             <Paper style={{ margin: 16, padding: 16 }}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                        <TextField placeholder="Add Todo here" fullWidth />
+                        <TextField 
+                            placeholder="Add Todo here" 
+                            fullWidth 
+                            onChange={this.onInputChange}
+                            onKeyPress={this.enterKeyEventHandler}
+                            value={this.state.item.title}
+                        />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary" variant="outlined">
+                        <Button fullWidth color="secondary" variant="outlined" onClick={this.onButtonClick}>
                             +
                         </Button>
                     </Grid>
--- src/App.js
+++ src/App.js
@@ -16,6 +16,17 @@ class App extends React.Component {
     };
   }
 
+  // 저장 이벤트
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 배열에 아이템 추가
+    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+    console.log("items : ", this.state.items);
+  };
+
+
   render() {
 
     let todoItems = this.state.items.length > 0 && (
@@ -32,10 +43,9 @@ class App extends React.Component {
       
       <div className="App">
         <Container maxWidth="md">
-          <AddTodo />
+          <AddTodo add={ this.add } />
           <div className="TodoList">{ todoItems }</div>
         </Container>
-        
       </div>
     );
   };
